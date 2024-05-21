eb43c73c52c42035729c3e2f814435cb5f8c9deb
unknown <phanvanphung6102001@gmail.com>
2024-05-21 18:20:04 +0700

    fe

---
+++ table/js/table_list.js
@@ -0,0 +1,21 @@
+const btn_menublock_edc = document.querySelectorAll(".btn_menublock_edc");
+const menu_edc = document.querySelectorAll(".menu_edc");
+
+
+btn_menublock_edc.forEach((e,index) => {
+    e.addEventListener("click", function () {
+        if (e.id === `button-${index + 1}`) {
+            menu_edc[index].style.display === "block" ? menu_edc[index].style.display = "none" : menu_edc[index].style.display = "block";
+        }
+    })
+
+})
+
+document.addEventListener("click", function (event) {
+    menu_edc.forEach((e,index) => {
+        if (!e.contains(event.target) && !btn_menublock_edc[index].contains(event.target)) {
+           e.style.display = "none";
+        }
+    })
+
+});
